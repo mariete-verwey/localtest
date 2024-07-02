43a203b4f6e6835959dce30173ad792de24dfa85
mariete-verwey <mariete.verwey@capgemini.com>
2024-07-02 11:18:09 +0200

    Changing ExampleComponent to show website

--- plugins/google/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/google/src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,38 +1,18 @@
 import React from 'react';
-import { Typography, Grid } from '@material-ui/core';
-import {
-  InfoCard,
-  Header,
-  Page,
-  Content,
-  ContentHeader,
-  HeaderLabel,
-  SupportButton,
-} from '@backstage/core-components';
-import { ExampleFetchComponent } from '../ExampleFetchComponent';
+import { Page, Header, Content } from '@backstage/core-components';
 
-export const ExampleComponent = () => (
-  <Page themeId="tool">
-    <Header title="Welcome to google!" subtitle="Optional subtitle">
-      <HeaderLabel label="Owner" value="Team X" />
-      <HeaderLabel label="Lifecycle" value="Alpha" />
-    </Header>
-    <Content>
-      <ContentHeader title="Plugin title">
-        <SupportButton>A description of your plugin goes here.</SupportButton>
-      </ContentHeader>
-      <Grid container spacing={3} direction="column">
-        <Grid item>
-          <InfoCard title="Information card">
-            <Typography variant="body1">
-              All content should be wrapped in a card like this.
-            </Typography>
-          </InfoCard>
-        </Grid>
-        <Grid item>
-          <ExampleFetchComponent />
-        </Grid>
-      </Grid>
-    </Content>
-  </Page>
-);
+export const ExampleComponent = () => {
+  return (
+    <Page themeId="tool">
+      <Content>
+        <iframe
+          src="https://www.google.com/webhp?igu=1"
+          title="a page"
+          width="100%"
+          height="100%"
+          style={{ border: 0 }}
+        />
+      </Content>
+    </Page>
+  );
+};
